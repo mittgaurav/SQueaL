d3dc3db8bd7d50eb29b5c3a1ad1fe80c6cfb7fea
mittgaurav <gm_5315@yahoo.com>
2019-05-18 22:38:07 +0530

    better comments

--- zynga_logging_warehousing.sql
+++ zynga_logging_warehousing.sql
@@ -1,3 +1,6 @@
+-- staging table level 1
+-- to get denormalized info
+-- around certain user id.
 DROP TABLE IF EXISTS play_level_1;
 CREATE TABLE play_level_1 as
 SELECT sessions.session_id, user_id, start_time, end_time, level, platform_name, location_x, location_y
@@ -7,7 +10,8 @@ SELECT sessions.session_id, user_id, start_time, end_time, level, platform_name,
        INNER JOIN clients 
          ON sessions.client_id = clients.client_id
  ORDER BY start_time, user_id, sessions.session_id;
-  
+
+-- age group dimension
 DROP TABLE IF EXISTS age_group_dim;
 CREATE TABLE age_group_dim(
 age_group VARCHAR(20),
@@ -27,6 +31,7 @@ INSERT INTO age_group_dim VALUES
                           ('past-prime', 60, 80), 
                           ('baroque', 80, 999);
 
+-- location dimension
 DROP TABLE IF EXISTS location_dim;
 CREATE TABLE location_dim(
 location_id INT PRIMARY KEY,
@@ -47,6 +52,7 @@ INSERT INTO location_dim (location_id, x, y, metro, country) VALUES
                          (8, 3, 2, TRUE, 'PAKISTAN'), 
                          (9, 3, 3, FALSE, 'INDIA');
 
+-- platform dimension
 DROP TABLE IF EXISTS platform_dim;
 CREATE TABLE platform_dim(
 platformid INT,
@@ -54,15 +60,22 @@ name       VARCHAR(100),
 mobile     BOOLEAN,
 company    VARCHAR(100)
 );
+
 INSERT INTO platform_dim VALUES
                          (1, 'iphone', TRUE, 'apple'), 
                          (2, 'android', TRUE, 'google'), 
                          (3, 'web', FALSE, 'microsoft'), 
                          (4, 'mac', FALSE, 'apple');
 
+-- final fact table with daily
+-- cummulative nums for sessions
+-- and duration by users, levels
+-- locations, and platforms. We
+-- will run queries on top of it
+DROP TABLE IF EXISTS play_fact;
 CREATE TABLE play_fact as
-SELECT date(play_level_1.start_time), user_id, level, platformid, location_id, age_group, 
-       sum(end_time - start_time) duration, count(session_id) sessions
+SELECT date(play_level_1.start_time) date, user_id, level, platformid, location_id, age_group, 
+       count(session_id) sessions, sum(end_time - start_time) duration
   FROM play_level_1
        INNER JOIN location_dim 
          ON x = location_x AND y = location_y
@@ -74,4 +87,4 @@ SELECT date(play_level_1.start_time), user_id, level, platformid, location_id, a
                INNER JOIN age_group_dim 
                  ON age >= start_age AND age < end_age) ages
          ON user_id = ages.id
- GROUP BY date(play_level_1.start_time), user_id, level, platformid, location_id, age_group;
+ GROUP BY date, user_id, level, platformid, location_id, age_group;
